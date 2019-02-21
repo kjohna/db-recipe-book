d96a501953acbde5075d18a045b69f904cc1fb5f
kjohna <43828763+kjohna@users.noreply.github.com>
2019-02-21 16:23:50 -0700

    JSON.stringify directions object

--- data/seeds/002-recipes.js
+++ data/seeds/002-recipes.js
@@ -9,7 +9,7 @@ exports.seed = function(knex, Promise) {
         // pizzas
         { name: 'pepperoni', 
           dish_id: 1, // id 1 for pizza
-          directions: {
+          directions: JSON.stringify({
             0: "oven 450F",
             1: "put dough",
             2: "put sauce",
@@ -17,11 +17,11 @@ exports.seed = function(knex, Promise) {
             4: "put pepperoni",
             5: "in oven 15mins",
             6: "cut it, eat it"
-          }, 
+          }), 
         },
         { name: 'sausage', 
           dish_id: 1, // id 1 for pizza
-          directions: {
+          directions: JSON.stringify({
             0: "oven 450F",
             1: "put dough",
             2: "put sauce",
@@ -29,11 +29,11 @@ exports.seed = function(knex, Promise) {
             4: "put sausage",
             5: "in oven 15mins",
             6: "cut it, eat it"
-          }, 
+          }), 
         },
         { name: 'anchovies', 
           dish_id: 1, // id 1 for pizza
-          directions: {
+          directions: JSON.stringify({
             0: "oven 450F",
             1: "put dough",
             2: "put sauce",
@@ -41,12 +41,12 @@ exports.seed = function(knex, Promise) {
             4: "put anchovies",
             5: "in oven 15mins",
             6: "cut it, eat it"
-          }, 
+          }), 
         },
         // tacos
         { name: 'beef tacos', 
           dish_id: 2, // id 2 for tacos
-          directions: {
+          directions: JSON.stringify({
             0: "onions, chopped",
             1: "beef, onions in pan till brown",
             2: "grate cheese",
@@ -54,50 +54,50 @@ exports.seed = function(knex, Promise) {
             4: "cut tomatoes",
             5: "warm shells",
             6: "cheese in shell, beef in shell, lettuce and tomatoes in shell, eat it"
-          }, 
+          }), 
         },
         { name: 'chicken tacos', 
           dish_id: 2, // id 2 for tacos
-          directions: {
+          directions: JSON.stringify({
             0: "chicken, onions in pot with broth till cooked",
             1: "grate cheese",
             2: "cut lettuce",
             3: "cut tomatoes",
             4: "warm shells",
             5: "cheese in shell, chicken in shell, lettuce and tomatoes in shell, eat it"
-          }, 
+          }), 
         },
         { name: 'pork tacos', 
           dish_id: 2, // id 2 for tacos
-          directions: {
+          directions: JSON.stringify({
             0: "pork in oven",
             1: "grate cheese",
             2: "cut lettuce",
             3: "cut tomatoes",
             4: "warm shells",
             5: "cheese in shell, pork in shell, lettuce and tomatoes in shell, eat it"
-          }, 
+          }), 
         },
         // soups
         { name: 'chicken soup', 
           dish_id: 3, // id 3 for soup
-          directions: {
+          directions: JSON.stringify({
             0: "celery, carrots, onion on stove med",
             1: "add chicken, broth",
             2: "add noodles, 10 minutes",
             3: "scoop it",
             4: "eat it",
-          }, 
+          }), 
         },
         { name: 'greek chicken soup', 
           dish_id: 3, // id 3 for soup
-          directions: {
+          directions: JSON.stringify({
             0: "celery, onion on stove med",
             1: "add chicken, broth",
             2: "add orzo, 10 minutes",
             3: "scoop it, add lemon juice, dill",
             4: "eat it",
-          }, 
+          }), 
         },
       ]);
     });
